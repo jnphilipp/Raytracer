546d3d76e6656d383b106f0f6e7565d760ecc2b8
jnphilipp <jnphilipp@yahoo.com>
2012-06-18 12:44:58 +0200

    Fixing shininess.

--- Shaders/PixelShader.c
+++ Shaders/PixelShader.c
@@ -7,13 +7,13 @@ void main() {
 	vec3 N = normalize(normal);
 	vec3 E = normalize(eyeVec);
 
-	for (int l=0; l<numLights; ++l) {
+	for ( int l = 0; l<numLights; ++l ) {
 		vec3 L = normalize(gl_LightSource[l].position.xyz - vertex);
 
-		if ( dot(N, L) > 0.0f ) {
+		if ( dot(N, L) > 0.0 ) {
 			vec3 R = reflect(-L, N);
-			float shin = 1.0f;
-			//float shin = pow(max(dot(R, E), 0.0f), gl_FrontMaterial.shininess);
+			//float shin = 1.0f;
+			float shin = pow(max(dot(R, E), 0.0), gl_FrontMaterial.shininess);
 
 			color.r += dot(N, L) * gl_LightSource[l].diffuse.r * gl_FrontMaterial.diffuse.r + gl_LightSource[l].specular.r * gl_FrontMaterial.specular.r * shin;
 			color.g += dot(N, L) * gl_LightSource[l].diffuse.g * gl_FrontMaterial.diffuse.g + gl_LightSource[l].specular.g * gl_FrontMaterial.specular.g * shin;
